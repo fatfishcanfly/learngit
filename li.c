cc9c30bc5cfb4508d0762948b53086bf65de05ef
lihui <19916936392@163.com>
2020-06-20 15:25:49 +0800

    & simple

--- li.c
+++ li.c
@@ -4,4 +4,5 @@ int main(void)
   printf("hello world");
   printf("smile every day");//this is a test file
   printf("test again"); // creating a new branch
+  /* creating a new branch is quick & simple */
 }
